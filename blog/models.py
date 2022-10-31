53138b341058d07494cc4aea30755e9d551e0f06
백정렬 <bjl5029@gmail.com>
2022-10-31 16:27:34 +0900

    "blog" 및 "single" 폴더 생성

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    create_at = models.DateTimeField
+    # author : 추후 작성 예정
