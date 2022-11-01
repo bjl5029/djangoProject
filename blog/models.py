db25792c082f74e5b10bd68dc934090964f59374
백정렬 <bjl5029@gmail.com>
2022-11-01 10:43:13 +0900

    블로그목록수정

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     content = models.TextField()
 
     create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
 
     def __str__(self):
